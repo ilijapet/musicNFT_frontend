650caee4b8c4c3a2ff3a315ad947743afc2d137c
ilija <ilijapet@gmail.com>
2023-12-09 20:12:16 +0100

    First front-end commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import SignUp from './components/SignUp';
+import SignInSide from './components/SignInSide';
+
+function App() {
+  return (
+      <SignInSide />
+    );
+}
+
+export default App;
