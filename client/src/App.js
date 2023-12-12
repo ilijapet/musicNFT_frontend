a0c2a58ab313acf9cfd0ba6d1ff10c27fb2b3bc5
ilija <ilijapet@gmail.com>
2023-12-12 22:26:00 +0100

    add build folder

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import SignUp from './components/SignUp';
 import SignInSide from './components/SignInSide';
 
 function App() {
-  console.log("u app sam")
+  console.log("unutra si")
   return (
       <SignInSide />
     );
