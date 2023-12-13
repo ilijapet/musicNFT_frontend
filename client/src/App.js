3f6aec172697d69de55bab886af0eca3570e2e6b
ilija <ilijapet@gmail.com>
2023-12-13 14:37:03 +0100

    test

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import SignUp from './components/SignUp';
 import SignInSide from './components/SignInSide';
 
 function App() {
-  console.log("nisi unutra")
+  console.log("Slusa moj kurac...") 
   return (
       <SignInSide />
     );
