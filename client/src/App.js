28c5aa986d99f169178e34dc2eeff5399213aa37
ilija <ilijapet@gmail.com>
2023-12-13 22:20:44 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import SignUp from './components/SignUp';
 import SignInSide from './components/SignInSide';
 
 function App() {
-  console.log("nije javno?..") 
+  console.log("Oce li to?..") 
   return (
       <SignInSide />
     );
