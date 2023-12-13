365867d7b4640b873f62c385aa6cf3b0244b0c33
ilija <ilijapet@gmail.com>
2023-12-13 14:00:50 +0100

    new build

--- client/src/components/SignInSide.js
+++ client/src/components/SignInSide.js
@@ -48,7 +48,7 @@ export default function SignInSide() {
     const [access, setAccess] = useState(null);
 
     const handleSubmit = async (event) => {   
-        console.log("Jel slusas?") 
+        console.log("Jel slusas? Slusa moj kurac...") 
         event.preventDefault();
         const data = new FormData(event.currentTarget);
         console.log({
