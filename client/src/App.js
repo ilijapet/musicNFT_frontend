c7f12f9dc72e6ee3735b353e449aed204b6d5bdf
ilija <ilijapet@gmail.com>
2023-12-14 09:43:53 +0100

    test

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import SignUp from './components/SignUp';
 import SignInSide from './components/SignInSide';
 
 function App() {
-  console.log("E mozda i ne?..") 
+  console.log("Hard and core..") 
   return (
       <SignInSide />
     );
